e4f2b3422da5d73707cd37deed2c60954e1f4a0a
dmitry.navitski <arbalestindigo@gmail.com>
2020-03-01 12:04:53 +0300

    Gradient view implementation

--- Gradient/ViewController.swift
+++ Gradient/ViewController.swift
@@ -9,12 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var alphaValue: CGFloat = 1.0
+    
+    private var roundFormatter: String = "%.2f"
+    
+    @IBOutlet weak var paletteView: UIView!
+    
+    @IBOutlet weak var redColorValue: UILabel!
+    @IBOutlet weak var greenColorValue: UILabel!
+    @IBOutlet weak var blueColorValue: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    private var viewColor: UIColor? {
+        didSet{
+            paletteView.backgroundColor = viewColor
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI();
+        setViewColor();
+    }
+    
+    @IBAction func sliderMoved(_ sender: UISlider) {
+        switch sender.tag {
+        case 1:
+            redColorValue.text = String(format: roundFormatter, sender.value)
+            break
+        case 2:
+            greenColorValue.text = String(format: roundFormatter, sender.value)
+            break
+        case 3:
+            blueColorValue.text = String(format: roundFormatter, sender.value)
+            break
+        default:
+            break
+        }
+        setViewColor();
     }
 
-
+    private func setupUI() {
+        paletteView.layer.cornerRadius = 10;
+        redColorValue.text = String(redSlider.value)
+        greenColorValue.text = String(greenSlider.value)
+        blueColorValue.text = String(blueSlider.value)
+    }
+    
+    private func setViewColor() {
+        viewColor = UIColor(displayP3Red: CGFloat(redSlider!.value), green: CGFloat(greenSlider!.value), blue: CGFloat(blueSlider!.value), alpha: alphaValue)
+    }
 }
 
